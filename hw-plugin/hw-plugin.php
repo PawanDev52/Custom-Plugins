c3d5f0489d969d86608623a21dd6ccdaaf3f852d
Pawan sharma <pawansh803@gmail.com>
2024-09-01 18:06:55 +0530

    Creating admin dashboard notice and widget

--- hw-plugin/hw-plugin.php
+++ hw-plugin/hw-plugin.php
@@ -1,4 +1,44 @@
 <?php
+/**
+ * Plugin Name: Hello World
+ * Description: This is our first plugin.
+ * Author: Pawan Sharma
+ * Version: 1.0
+ * Author URI: https://example.com
+ * Plugin URI: https://example.com/hello-world
+ */
 
+//  Admin Dashboard Notices
+// i am commenting this entire code because i am pushing it on github so when clonning it uncomment the code.
+/*
+add_action("admin_notices", "hw_show_warning_message");
+
+function hw_show_message(){
+    echo '<div class="notice notice-success is-dismissible"><p>Hello, I am a success message.</p></div>';
+}
+
+function hw_error_message(){
+    echo '<div class="notice notice-error is-dismissible"><p>Hello, I am an error message.</p></div>';
+}
+
+function hw_show_info_message(){
+    echo '<div class="notice notice-info is-dismissible"><p>Hello, I am an informational message.</p></div>';
+}
+
+function hw_show_warning_message(){
+    echo '<div class="notice notice-warning is-dismissible"><p>Hello, I am a warning message.</p></div>';
+}
+
+// Admin Dashboard Widgets
+add_action("wp_dashboard_setup", "hw_dashboard_widget");
+
+function hw_dashboard_widget(){
+    wp_add_dashboard_widget("hw_hellow_world", "HW - Hello World Widget", "hw_custom_admin_widget");
+}
+
+function hw_custom_admin_widget(){
+    echo "This is a custom widget of hello world plugin.";
+}
+*/
 
 ?>
