513300674a7e6b9dd0919a1066cef8d80b7e719b
Pawan sharma <pawansh803@gmail.com>
2024-09-11 22:23:36 +0530

    csv data uploader

--- csv-data-uploader/csv-data-uploader.php
+++ csv-data-uploader/csv-data-uploader.php
@@ -1,4 +1,11 @@
 <?php
 
+/**
+ * Plugin Name: CSV Data Uploader
+ * Description: This plugin will upload CSV data to DB table
+ * Author: Pawan Sharma
+ * Plugin URI: https://example.com/csv-data-uploader
+ * Author URI: https://example.com
+ */
 
 ?>
