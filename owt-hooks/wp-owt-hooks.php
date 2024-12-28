271c2e4cd8f3ab65ada0480a119a2ee6731a844e
Pawan sharma <pawansh803@gmail.com>
2024-12-28 18:06:55 +0530

    action hooks

--- owt-hooks/wp-owt-hooks.php
+++ owt-hooks/wp-owt-hooks.php
@@ -9,20 +9,22 @@
 
  /*
 
- function owt_wp_init(){
+function owt_wp_init()
+{
 
     $args = array(
         'public' => true,
         'label' => 'OWT Hooks'
     );
     register_post_type('owt_hook', $args);
- }
+}
 
 //  add_action("init", "owt_wp_init");
 
 
 // action hook - widgets_init
-function owt_register_sidebar(){
+function owt_register_sidebar()
+{
     register_sidebar(array(
         'name' => __('OWT sidebar'),
         'id' => 'owt-sidebar-1',
@@ -38,16 +40,19 @@ function owt_register_sidebar(){
 
 
 // action hook = admin_menu
-function owt_custom_menu(){
+function owt_custom_menu()
+{
     add_menu_page("OWT Playlist", "OWT playlist", "manage_options", "owt-playlist", "owt_plylist_fn");
     add_submenu_page("owt-playlist", "Submenu 1", "Submenu 1", "manage_options", "submenu-1", "owt_submenu_1_fn");
 }
 
-function owt_plylist_fn(){
+function owt_plylist_fn()
+{
     echo "This is our admin menu page";
 }
 
-function owt_submenu_1_fn(){
+function owt_submenu_1_fn()
+{
     echo "This is our first submenu page";
 }
 
@@ -55,20 +60,131 @@ function owt_submenu_1_fn(){
 
 
 // action hook - admin_enqueue_scripts & wp_enqueue_scripts
-function owt_attach_assets_to_admin(){
+function owt_attach_assets_to_admin()
+{
     // functions for adding css and js files
-    wp_enqueue_style("owt-css", plugin_dir_url(__FILE__) . "assets/css/owt-admin.css"); 
-    wp_enqueue_script("owt-js", plugin_dir_url(__FILE__) . "assets/js/owt-admin.js"); 
+    wp_enqueue_style("owt-css", plugin_dir_url(__FILE__) . "assets/css/owt-admin.css");
+    wp_enqueue_script("owt-js", plugin_dir_url(__FILE__) . "assets/js/owt-admin.js");
 }
 
 // add_action("admin_enqueue_scripts", "owt_attach_assets_to_admin");
 
-function owt_attach_assets_to_front(){
+function owt_attach_assets_to_front()
+{
     // functions for adding css and js files
-    wp_enqueue_style("owt-css", plugin_dir_url(__FILE__) . "assets/css/owt-front.css"); 
-    wp_enqueue_script("owt-js", plugin_dir_url(__FILE__) . "assets/js/owt-front.js"); 
+    wp_enqueue_style("owt-css", plugin_dir_url(__FILE__) . "assets/css/owt-front.css");
+    wp_enqueue_script("owt-js", plugin_dir_url(__FILE__) . "assets/js/owt-front.js");
 }
 
 // add_action("wp_enqueue_scripts", "owt_attach_assets_to_front");
+
+
+// action hook - admin_bar_menu
+function owt_custom_bar_menu($wp_admin_bar)
+{
+    // for adding menu and sub menu in the dashboard admin bar
+    $args = array(
+        "id" => "owt-blog",
+        "title" => "Custom bar menu",
+        "href" => "https://github.com/PawanDev52",
+        "meta" => array(
+            "class" => "owt-custom-blog",
+            "target" => "_blank"
+        )
+    );
+
+    $wp_admin_bar->add_node($args);
+
+    $submenu1 = array(
+        "id" => "owt-submenu1",
+        "title" => "Google",
+        "href" => "https:www.google.com",
+        "parent" => "owt-blog"
+    );
+
+    $wp_admin_bar->add_node($submenu1);
+
+    $submenu2 = array(
+        "id" => "owt-submenu2",
+        "title" => "Youtube",
+        "href" => "https:www.youtube.com",
+        "parent" => "owt-blog",
+        "meta" => array(
+            "target" => "_blank"
+        )
+    );
+
+    $wp_admin_bar->add_node($submenu2);
+}
+
+// add_action("admin_bar_menu", "owt_custom_bar_menu", 999);
+
+
+// action hook - admin_notices
+function owt_admin_notice()
+{
+?>
+    <div class="notice notice-error is-dismissible">
+        <p>This is an error message</p>
+    </div>
+<?php
+}
+
+// add_action("admin_notices", "owt_admin_notice");
+
+
+// action hook - add_meta_boxes
+function owt_make_mbx()
+{
+
+    add_meta_box(
+        'owt-mbx',
+        'OWT Custom Box',
+        'owt_mbx_fn',
+        'post',
+        'side',
+        'high'
+    );
+}
+
+function owt_mbx_fn($post)
+{
+    echo "This is a custom meta box";
+?>
+    <div>
+        <label>Name</label>
+        <input type="text" value="<?php echo get_post_meta($post->ID, "owt_mx_value", true); ?>" name="owt_mbx_name" placeholder="Enter Name">
+    </div>
+<?php
+}
+
+// add_action("add_meta_boxes", "owt_make_mbx");
+
+
+// action hook - save_post  this hook is working with the above hook add_meta_boxes because it saves the data from that meta box input field
+// add_action("save_post", "owt_save_mbx_value");
+
+function owt_save_mbx_value($post_id)
+{
+
+    $owt_mbx_name = isset($_REQUEST['owt_mbx_name']) ? trim($_REQUEST['owt_mbx_name']) : "";
+
+    if (!empty($owt_mbx_name)) {
+
+        update_post_meta($post_id, "owt_mx_value", $owt_mbx_name);
+    }
+}
+
+
+// action hook - login_enqueue_scripts
+function owt_attach_assets_to_login_page()
+{
+    // functions for adding css and js files
+    wp_enqueue_style("owt-css1", plugin_dir_url(__FILE__) . "assets/css/owt-login.css");
+    wp_enqueue_script("owt-js1", plugin_dir_url(__FILE__) . "assets/js/owt-login.js");
+}
+
+add_action("login_enqueue_scripts", "owt_attach_assets_to_login_page");
+
 
 */
