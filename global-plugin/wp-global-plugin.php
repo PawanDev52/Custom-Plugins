d7fab5af74e09fb4f3bdb8a57b308d0483c7b67c
Pawan sharma <pawansh803@gmail.com>
2025-01-08 22:43:00 +0530

    global variables

--- global-plugin/wp-global-plugin.php
+++ global-plugin/wp-global-plugin.php
@@ -1,6 +1,26 @@
 <?php
+
 /**
  * Plugin name: Global Plugin
  * Author: Pawan
  * Description: This is a custom plugin for global variables
  */
+
+ /*
+ function wp_global_attach_menu(){
+
+    add_menu_page("Global OWT", "Global OWT", "manage_options", "global-menu", "wpl_fn_global_menu");
+} 
+
+function wpl_fn_global_menu(){
+    echo "<h2>Welcome here!</h2>";
+
+    global $wpdb, $wp_locale, $wp_roles, $wp_registered_sidebars;
+    echo "<pre>";
+    print_r($wp_registered_sidebars);
+}
+
+// add_action("admin_menu", "wp_global_attach_menu");
+
+*/
+?>
