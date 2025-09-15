b04c5ffe76e489052948f6f9b58e05cfc7de862b
Pawan sharma <pawansh803@gmail.com>
2025-09-15 22:55:59 +0530

    admin notice and widget

--- hw-plugin/hw-plugin.php
+++ hw-plugin/hw-plugin.php
@@ -13,6 +13,8 @@
 
 //  Admin Dashboard Notices
 // i am commenting this entire code because i am pushing it on github so when clonning it uncomment the code.
+
+// showing notice and widgets in the admin dashboard
 /*
 add_action("admin_notices", "hw_show_warning_message");
 
