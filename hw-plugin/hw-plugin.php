1f06df35c39ddf0e9631a999959ac409e778d3e0
Pawan sharma <pawansh803@gmail.com>
2025-09-23 22:51:05 +0530

    admin dashboard notices and widgets

--- hw-plugin/hw-plugin.php
+++ hw-plugin/hw-plugin.php
@@ -2,48 +2,49 @@
 
 /**
  * Plugin Name: Hello World
- * Description: This is our first plugin.
+ * Description: This is a plugin which creates information widgets and admin notices in the admin dashboard.
  * Author: Pawan Sharma
  * Version: 1.0
- * Author URI: https://example.com
- * Plugin URI: https://example.com/hello-world
+ * Author URI: https://github.com/PawanDev52
+ * Plugin URI: https://github.com/PawanDev52/Custom-Plugins
  */
 
-// commenting all code
-
-//  Admin Dashboard Notices
-// i am commenting this entire code because i am pushing it on github so when clonning it uncomment the code.
-
-// Plugin for showing notices and widgets in the admin dashboard
 /*
-add_action("admin_notices", "hw_show_warning_message");
+//  Admin Dashboard Notices
+add_action("admin_notices", "hw_info_message");
 
-function hw_show_message(){
-    echo '<div class="notice notice-success is-dismissible"><p>Hello, I am a success message.</p></div>';
+function hw_show_message()
+{
+    echo '<div class="notice notice-success is-dismissible"><p>This is a success message</p></div>';
 }
 
-function hw_error_message(){
-    echo '<div class="notice notice-error is-dismissible"><p>Hello, I am an error message.</p></div>';
+function hw_warning_message()
+{
+    echo '<div class="notice notice-warning is-dismissible"><p>This is a warning message</p></div>';
 }
 
-function hw_show_info_message(){
-    echo '<div class="notice notice-info is-dismissible"><p>Hello, I am an informational message.</p></div>';
+function hw_error_message()
+{
+    echo '<div class="notice notice-error is-dismissible"><p>This is an error message</p></div>';
 }
 
-function hw_show_warning_message(){
-    echo '<div class="notice notice-warning is-dismissible"><p>Hello, I am a warning message.</p></div>';
+function hw_info_message()
+{
+    echo '<div class="notice notice-info is-dismissible"><p>This is an information message</p></div>';
 }
+
 
 // Admin Dashboard Widgets
-function hw_dashboard_widget(){
-    wp_add_dashboard_widget("hw_hellow_world", "HW - Hello World Widget", "hw_custom_admin_widget");
-}
-
-function hw_custom_admin_widget(){
-    echo "This is a custom widget of hello world plugin.";
-}
-
 add_action("wp_dashboard_setup", "hw_dashboard_widget");
-*/
 
+function hw_dashboard_widget()
+{
+    wp_add_dashboard_widget("hw_dashboard_widget", "Hw - Dashboard Widget", "hw_custom_widget");
+}
+
+function hw_custom_widget()
+{
+    echo 'This is a custom admin dashboard widget!';
+}
+*/
 ?>
