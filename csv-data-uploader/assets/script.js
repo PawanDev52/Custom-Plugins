d5b4bf6439671a40872d8afeea3d28e6c25da308
Pawan sharma <pawansh803@gmail.com>
2025-10-10 22:57:41 +0530

    csv data uploader

--- csv-data-uploader/assets/script.js
+++ csv-data-uploader/assets/script.js
@@ -1,7 +1,20 @@
-jQuery(document).ready(function () {
-  jQuery("#frm-csv-upload").on("submit", function (event) {
-    event.preventDefault();
+// jQuery(document).ready(function () {
+//   jQuery("#frm-csv-upload").on("submit", function (event) {
+//     event.preventDefault();
 
-    
-  });
-});
+//     // jQuery.ajax({
+//     //     url,
+//     //     data,
+//     //     datatype,
+//     //     method,
+//     //     processData,
+//     //     contentType,
+//     //     success: function(){
+
+//     //     }
+//     // });
+//   });
+// });
+
+
+console.log("this is script file");
