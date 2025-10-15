14be878bd566210c067578858cb7d865e2fe0580
Pawan sharma <pawansh803@gmail.com>
2025-10-15 23:05:16 +0530

    db data download

--- db-data-csv-backup/db-data-csv-backup.php
+++ db-data-csv-backup/db-data-csv-backup.php
@@ -2,6 +2,10 @@
 
 /**
  * Plugin Name: CSV Data Backup
+ * Author: Pawan Sharma
+ * Version: 1.0
+ * Author URI: https://github.com/PawanDev52
+ * Plugin URI: https://github.com/PawanDev52/Custom-Plugins
  */
 
 
