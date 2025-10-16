59311d959e83bc9212bb76f87cb956b62f5109e6
Pawan sharma <pawansh803@gmail.com>
2025-10-16 22:22:35 +0530

    csv data backup

--- db-data-csv-backup/db-data-csv-backup.php
+++ db-data-csv-backup/db-data-csv-backup.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name: CSV Data Backup
+ * Description: Database table data backup in csv format
  * Author: Pawan Sharma
  * Version: 1.0
  * Author URI: https://github.com/PawanDev52
  * Plugin URI: https://github.com/PawanDev52/Custom-Plugins
  */
-
 
 ?>
