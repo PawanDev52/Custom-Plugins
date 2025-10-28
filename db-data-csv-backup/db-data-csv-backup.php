f21e3b5d56de1bcb50e2fe46904be010725fd33f
Pawan sharma <pawansh803@gmail.com>
2025-10-28 22:32:30 +0530

    csv data backup

--- db-data-csv-backup/db-data-csv-backup.php
+++ db-data-csv-backup/db-data-csv-backup.php
@@ -17,8 +17,10 @@
 //  add_menu_page("CSV Data Backup Plugin", "CSV Data Backup", "manage_options", "csv-data-backup", "tdcb_export_form", "dashicons-database-export", 8);
 // }
 
-function tdcb_export_form() {
-  echo "<h3>DB Table Data CSV Backup Plugin</h3>";
-}
+// function tdcb_export_form() {
+//   echo "<h3>DB Table Data CSV Backup Plugin</h3>";
+// }
+
+// next steps for the plugin
 
 ?>
